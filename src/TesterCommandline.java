f7ad3bc43e5fb1faaf6b27359604bff7bb70f8b5
Branden- <brandenandersen1@gmail.com>
2015-10-18 23:31:17 -0700

    Some Minor additions to the commandline tester so it would work with intellij

--- src/TesterCommandline.java
+++ src/TesterCommandline.java
@@ -4,11 +4,11 @@ import java.io.FileNotFoundException;
  * Created by Branden on 10/18/2015.
  */
 public class TesterCommandline {
-    public static void main() throws FileNotFoundException {
-        String[] args = {"-a", "hamlet.txt"};
+    public static void main(String args[]) throws FileNotFoundException {
+        String[] someArgs = {"-a", "hamlet.txt"};
 
         //test if a proper error message is printed
-        WordCount.main(args);
+        WordCount.main(someArgs);
 
     }
 
