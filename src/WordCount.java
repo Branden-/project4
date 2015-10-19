a3af6fcefd4c058d634ff98b1efa3448884752ab
branden <brandenandersen1@gmail.com>
2015-10-19 05:01:52 -0700

    added merge sort algorithm (not working) and a tester for merge sort. Need to work out how to declare a generic array.

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 /**
@@ -75,10 +76,6 @@ public class WordCount {
     	 	default: counter = new HashTable();
     	 }
 
-
-
-
-    	 
         try {
             FileWordReader reader = new FileWordReader(file);
             String word = reader.nextWord();
@@ -146,9 +143,9 @@ public class WordCount {
     }
 
     /**
-     *
-     * @param counts
-     * @param <E>
+     * Sort the count array in descending order using the quick sort algorithm
+     * @param counts an array containing a count of the words and the word associated with count
+     * @param <E> generic data type
      */
     private static <E extends Comparable<? super E>> void quickSortByDescendingCount(
             DataCount<E>[] counts) {
@@ -156,13 +153,86 @@ public class WordCount {
     }
 
     /**
-     *
-     * @param counts
+     * Sort the count array in descending order using the merge sort algorithm.
+     * @param counts an array containing a count of the words and the word associated with count
      * @param <E>
      */
     private static <E extends Comparable<? super E>> void mergeSortByDescendingCount(
             DataCount<E>[] counts) {
-        //STUB put your code here
+        DataCount<E> [] tmpArray = (DataCount<E> []) new DataCount<E>()[counts.length];
+
+        mergeSortDescending(counts, tmpArray, 0, counts.length - 1);
+    }
+
+    /**
+     * Internal method for the mergesort algorithm that makes recursive calls
+     * that divide and conquer the array
+     * @param a an array of Comparable items.
+     * @param tmpArray an array to place the merged result.
+     * @param left the left-most index of the subArray
+     * @param right the right-most index of the subarray
+     * @param <E> generic data type
+     */
+    private static <E extends Comparable<? super E>>
+    void mergeSortDescending(DataCount<E> [] a, DataCount<E> [] tmpArray, int left, int right){
+        if(left < right) {
+            int center = (left + right) / 2;
+            mergeSortDescending(a, tmpArray, left, center);
+            mergeSortDescending(a, tmpArray, center + 1, right);
+            merge(a, tmpArray, left, center + 1, right);
+        }
+    }
+
+    /**
+     * Internal method that merges two sorted halves of a subarray.
+     * @param a an array of Comparable items.
+     * @param tmpArray an array to place the merged result.
+     * @param leftPos the left-most index of the subarray.
+     * @param rightPos the index of the start of the second half.
+     * @param rightEnd the right-most index of the subarray
+     * @param <E>
+     */
+    private static <E extends Comparable<? super E>>
+    void merge(DataCount<E> [] a, DataCount<E> [] tmpArray, int leftPos, int rightPos, int rightEnd){
+        int leftEnd = rightPos - 1;
+        int tmpPos = leftPos;
+        int numElements = rightEnd - leftPos + 1;
+
+        //cycle through sub arrays and do comparisons filling the tmpArray
+        //this does our real sorting
+        while( leftPos <= leftEnd && rightPos <= rightEnd ){ //sort according to count, larger first
+            if( a[leftPos].count > a[rightPos].count ){
+                tmpArray [ tmpPos++ ] = a[ leftPos++ ];
+            }
+            else if( a[leftPos].count == a[rightPos].count ){ //counts are equal, sort alphabetically
+                if( a[leftPos].data.compareTo( a[rightPos].data ) >= 0 ){
+                    tmpArray [ tmpPos++ ] = a[ leftPos++ ];
+                }
+                else {
+                    tmpArray [ tmpPos++ ] = a[ rightPos++ ];
+                }
+            }
+            else {
+                tmpArray [ tmpPos++ ] = a[ rightPos++ ];
+            }
+        }
+
+        //Copy rest of first half if any left
+        while( leftPos <= leftEnd ){
+            tmpArray[ tmpPos++ ] = a[ leftPos++ ];
+        }
+
+        //copy rest of right half if any left
+        while( rightPos <= rightEnd ){
+            tmpArray[ tmpPos++ ] = a[ leftPos++ ];
+        }
+
+        //Copy tmpArray back
+        for(int i = 0; i < numElements; i++, rightEnd-- ){
+            a[ rightEnd ] = tmpArray[ rightEnd ];
+        }
+
+
     }
 
 
