a5b8825956f76420e12ed4814fffdfc435f7ca5d
anhqpho <papillon710@gmail.com>
2015-10-21 18:41:53 -0700

    Added Sorting Performance Test

--- src/Project3.java
+++ src/Project3.java
@@ -1,4 +1,5 @@
 import java.io.FileNotFoundException;
+import java.util.concurrent.TimeUnit;
 
 public class Project3 {
 
@@ -7,7 +8,8 @@ public class Project3 {
 		//testAVL();
 		//testHashTable();
 		//testWordCount();
-		testDocCorrelator();
+		//testDocCorrelator();
+		testSortPerformance();
 	}
 
 	
@@ -40,6 +42,24 @@ public class Project3 {
 	static void testDocCorrelator(){
 		String[] cmd = {"completeWorksofShakespeare.txt", "hamlet.txt", "-h"};
 		DocCorrelator.main(cmd);
+	}
+	
+	static void testSortPerformance(){
+		String[] sortType = {"-is", "-qs", "-ms"};
+		long[] startTime = new long[3];
+		long[] endTime = new long[3];
+		long[] durTime = new long[3];
+		String file = "completeWorksofShakespeare.txt";
+		
+		for (int i = 0; i < 3; i++){
+			startTime[i] = System.nanoTime();
+			WordCount.countWords("-b", sortType[i], file);
+			endTime[i] = System.nanoTime();
+			durTime[i] = endTime[i] - startTime[i];
+			System.out.println("For file: " + file + " sort type of " + sortType[i] + " take(ms): " + TimeUnit.NANOSECONDS.toMillis(durTime[i]));
+		}
+				
+				
 	}
 	
 	
